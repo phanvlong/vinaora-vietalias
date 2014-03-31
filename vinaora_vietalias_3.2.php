ca58802d67eb452e5d9dc4086653526e7172d218
phanvlong <long@phanvan.com>
2014-04-01 00:08:45 +0700

    Create vinaora_vietalias_3.2.php
    Plugin chỉ chạy được với joomla 2.x, để có thể chạy được với joomla 3.2.x cần sửa 2 dòng sau trong file vinaora_vietalias.php
    
    line 41: thay DS bằng DIRECTORY_SEPARATOR
    line 56: thay &JFactory bằng JFactory

---
+++ vinaora_vietalias_3.2.php
@@ -0,0 +1,61 @@
+<?php
+ /**
+ * @version		$Id: vinaora_vietalias.php 2012-06-17 vinaora $
+ * @package		Vinaora Vietnamese Alias
+ * @subpackage	plg_system_vinaora_vietalias
+ * @copyright	Copyright (C) 2010-2012 VINAORA. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @website		http://vinaora.com
+ * @twitter		http://twitter.com/vinaora
+ * @facebook	http://facebook.com/vinaora
+ * @google+		https://plus.google.com/111142324019789502653
+ *
+ * @note		See more details >> http://en.wikipedia.org/wiki/Vietnamese_alphabet
+ */
+ 
+// no direct access
+defined('_JEXEC') or die;
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgSystemVinaora_VietAlias extends JPlugin
+{
+	var $layout = '';
+	var $option = '';
+
+	function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		
+		$this->layout = JRequest::getCmd('layout');
+		$this->option = JRequest::getCmd('option');
+
+		if ( $this->layout != 'edit') return;
+		
+		$active = (bool) $this->params->get('active_on');
+		$pages	= $this->params->get('active_on_specific');
+		
+		if( $active || (strpos($pages, $this->option) !== false) )
+		{
+			require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'output.php';
+		}
+
+	}
+
+	function onAfterDispatch()
+	{
+		
+		if ( $this->layout != 'edit') return false;
+		
+		$auto_complete	= (bool) $this->params->get('auto_complete');
+		$pages			= $this->params->get('auto_complete_on_specific');
+		
+		if( $auto_complete || (strpos($pages, $this->option) !== false) )
+		{
+			$document = JFactory::getDocument();
+			$document->addScript( rtrim(JURI::root( true ), '/').'/media/plg_system_vinaora_vietalias/js/vinaora_vietalias.js' );
+		}
+		return true;
+	}
+}
